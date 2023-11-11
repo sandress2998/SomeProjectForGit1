886df1357cb8d016b5833d98a4f352f2752a59bf
sandress2998 <sandress2000@mail.ru>
2023-11-11 22:38:53 +0300

    Я не знаю, что делать, пожалуйста, работай!

--- app/src/main/java/com/example/someprojectforgit/MainActivity.kt
+++ app/src/main/java/com/example/someprojectforgit/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SomeProjectForGitTheme {
-                // A surface contgfgfgававfainer using the 'background' color from the theme
+                // A surface contgfg using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
